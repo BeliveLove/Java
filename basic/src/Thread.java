34beb3f678d774ee06c27c0e178ebc0096ac8acd
YYB <1755916756@qq.com>
2020-10-20 12:40:46 +0800

    Java基础

--- basic/src/Thread.java
+++ basic/src/Thread.java
@@ -13,7 +13,7 @@ public class Thread {
         System.out.println("new状态");
         byte[] lock = new byte[0];
         MyThread thread = new MyThread(lock);
-        System.out.println(thread.getName()+"      "+thread.getState());
+        System.out.println(thread.getName() + "      " + thread.getState());
     }
 
     private static void RUNNABLE() {
@@ -26,11 +26,12 @@ public class Thread {
     }
 
     private static void BLOCKED() {
-        //线程等待获取锁的状态
+        //block:线程等待获取锁的状态(阻塞态)
         byte[] lock = new byte[0];
         //此时锁的优先权在thread1
         MyThread thread1 = new MyThread(lock);
         thread1.start();
+
         //当thread1结束时，释放所资源，thread2获取锁
         MyThread thread2 = new MyThread(lock);
         thread2.start();
@@ -39,11 +40,16 @@ public class Thread {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println(thread2.getName()+"       "+thread2.getState());
+        //耗时等待态
+        System.out.println(thread1.getName() + "       " + thread1.getState());
+        //阻塞态    此时锁的优先权在thread1，不在thread2，thread2处于阻塞态
+        System.out.println(thread2.getName() + "       " + thread2.getState());
+
     }
 
     public static class MyThread extends java.lang.Thread {
         private byte[] lock = new byte[0];
+
         public MyThread(byte[] lock) {
             this.lock = lock;
         }
@@ -56,8 +62,26 @@ public class Thread {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println(this.getName()+"     "+"done");
+                System.out.println(this.getName() + "     " + "done");
             }
+        }
+    }
+
+    public static class MyThread2 extends java.lang.Thread {
+        private byte[] lock = new byte[0];
+
+        public MyThread2(byte[] lock) {
+            this.lock = lock;
+        }
+
+        @Override
+        public void run() {
+            try {
+                java.lang.Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(this.getName() + "     " + "done");
         }
     }
 }
