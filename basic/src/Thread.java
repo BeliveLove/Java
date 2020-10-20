3f74fbef5ca58aa6ba58a1c4e995d00309715764
YYB <1755916756@qq.com>
2020-10-20 14:14:35 +0800

    Java基础

--- basic/src/Thread.java
+++ basic/src/Thread.java
@@ -1,3 +1,6 @@
+
+import java.util.concurrent.locks.LockSupport;
+
 /**
  * @author Administrator
  */
@@ -6,6 +9,8 @@ public class Thread {
         NEW();
         RUNNABLE();
         BLOCKED();
+        WAITING();
+        TERMINATED();
     }
 
     private static void NEW() {
@@ -13,7 +18,8 @@ public class Thread {
         System.out.println("new状态");
         byte[] lock = new byte[0];
         MyThread thread = new MyThread(lock);
-        System.out.println(thread.getName() + "      " + thread.getState());
+        //NEW
+        System.out.println(thread.getName() + "        " + thread.getState());
     }
 
     private static void RUNNABLE() {
@@ -22,16 +28,17 @@ public class Thread {
         byte[] lock = new byte[0];
         java.lang.Thread thread = new MyThread(lock);
         thread.start();
-        System.out.println(thread.getName() + "    " + thread.getState());
+        //RUNNABLE
+        System.out.println(thread.getName() + "        " + thread.getState());
     }
 
     private static void BLOCKED() {
+        System.out.println("block状态");
         //block:线程等待获取锁的状态(阻塞态)
         byte[] lock = new byte[0];
         //此时锁的优先权在thread1
         MyThread thread1 = new MyThread(lock);
         thread1.start();
-
         //当thread1结束时，释放所资源，thread2获取锁
         MyThread thread2 = new MyThread(lock);
         thread2.start();
@@ -41,10 +48,53 @@ public class Thread {
             e.printStackTrace();
         }
         //耗时等待态
-        System.out.println(thread1.getName() + "       " + thread1.getState());
+        //TIMED_WAITING
+        System.out.println(thread1.getName() + "        " + thread1.getState());
         //阻塞态    此时锁的优先权在thread1，不在thread2，thread2处于阻塞态
-        System.out.println(thread2.getName() + "       " + thread2.getState());
+        //BLOCKED
+        System.out.println(thread2.getName() + "        " + thread2.getState());
 
+    }
+
+    public static void WAITING() {
+        /*
+         * WAITING分为
+         *   1.TIMED_WAITING：耗时等待态 限时条件往下走
+         *   2.WAITING：永久等待态 符合条件往下走
+         * */
+        System.out.println("waiting状态");
+        byte[] lock = new byte[0];
+        MyThread2 Thread3 = new MyThread2(lock);
+        Thread3.start();
+        try {
+            java.lang.Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread3.getName() + "        " + Thread3.getState());
+        LockSupport.unpark(Thread3);
+        try {
+            java.lang.Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread3.getName() + "        " + Thread3.getState());
+    }
+
+    public static void TERMINATED() {
+        System.out.println("terminated状态");
+        /*
+         * TIMED_WAITING:线程结束的状态
+         * */
+        byte[] lock = new byte[0];
+        MyThread myThread = new MyThread(lock);
+        myThread.start();
+        try {
+            java.lang.Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(myThread.getName() + "        " + myThread.getState());
     }
 
     public static class MyThread extends java.lang.Thread {
@@ -76,12 +126,8 @@ public class Thread {
 
         @Override
         public void run() {
-            try {
-                java.lang.Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(this.getName() + "     " + "done");
+            LockSupport.park();
+            System.out.println(this.getName() + "        " + "done");
         }
     }
 }
