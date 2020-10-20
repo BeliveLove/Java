434f2bfb5cb1a8dff95825761d2532d9e8f2dde4
YYB <1755916756@qq.com>
2020-10-20 12:19:28 +0800

    Java基础

--- basic/src/Thread.java
+++ basic/src/Thread.java
@@ -9,6 +9,7 @@ public class Thread {
     }
 
     private static void NEW() {
+        //new:new了线程，但是未start
         System.out.println("new状态");
         byte[] lock = new byte[0];
         MyThread thread = new MyThread(lock);
@@ -16,6 +17,7 @@ public class Thread {
     }
 
     private static void RUNNABLE() {
+        //run：new了线程，启用start
         System.out.println("run状态");
         byte[] lock = new byte[0];
         java.lang.Thread thread = new MyThread(lock);
@@ -24,9 +26,12 @@ public class Thread {
     }
 
     private static void BLOCKED() {
+        //线程等待获取锁的状态
         byte[] lock = new byte[0];
+        //此时锁的优先权在thread1
         MyThread thread1 = new MyThread(lock);
         thread1.start();
+        //当thread1结束时，释放所资源，thread2获取锁
         MyThread thread2 = new MyThread(lock);
         thread2.start();
         try {
@@ -39,7 +44,6 @@ public class Thread {
 
     public static class MyThread extends java.lang.Thread {
         private byte[] lock = new byte[0];
-
         public MyThread(byte[] lock) {
             this.lock = lock;
         }
